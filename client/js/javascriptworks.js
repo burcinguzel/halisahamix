a507eeaa40589fb4664dd20c0ba1af158cccc0c5
purch <burcinguzel@engineer.com>
2017-04-05 08:44:23 +0000

    2nd commit

--- client/js/javascriptworks.js
+++ client/js/javascriptworks.js
@@ -1,3 +1,29 @@
+var orderState  = 0;
+$("#btn222").click(function() {
+  $("#inpDef").attr("rows","4");
+  $("#inpMid").attr("rows","4");
+  $("#inpFor").attr("rows","4");
+  $(this).removeClass("btn-success").addClass("btn-danger");
+  $(this).siblings().removeClass("btn-danger").addClass("btn-success");
+  orderState=0;
+});
+$("#btn321").click(function() {
+  $("#inpDef").attr("rows","6");
+  $("#inpMid").attr("rows","4");
+  $("#inpFor").attr("rows","2");
+  $(this).removeClass("btn-success").addClass("btn-danger");
+  $(this).siblings().removeClass("btn-danger").addClass("btn-success");
+  orderState=1;
+});
+$("#btn231").click(function() {
+  $("#inpDef").attr("rows",4);
+  $("#inpMid").attr("rows",6);
+  $("#inpFor").attr("rows",2);
+  $(this).removeClass("btn-success").addClass("btn-danger");
+  $(this).siblings().removeClass("btn-danger").addClass("btn-success");
+  orderState=2;
+});
+
 
 var svg = d3.select("#svcField").append("g");
 var kadrolar ={}
@@ -5,29 +31,39 @@ var kadrolar ={}
 
 $("#btnMix").click(function(){
     createTeams();
+  var fieldOrder= [
+                    [[170,32,kadrolar.kaleciler[0]],
+                    [100,100,kadrolar.defanslar[0]], [240,100,kadrolar.defanslar[1]],
+                    [60,180,kadrolar.ortasahalar[0]],  [280,180,kadrolar.ortasahalar[1]],
+                    [100,260,kadrolar.forvetler[0]], [240,260,kadrolar.forvetler[1]],
+                    [100,370,kadrolar.forvetler[2]], [240,370,kadrolar.forvetler[3]],
+                    [60,450,kadrolar.ortasahalar[2]],  [280,450,kadrolar.ortasahalar[3]],
+                    [100,530,kadrolar.defanslar[2]], [240,530,kadrolar.defanslar[3]],
+                    [170,608,kadrolar.kaleciler[1]]
+                    ],
+                    [[170,32,kadrolar.kaleciler[0]],
+                    [60,100,kadrolar.defanslar[0]], [170,100,kadrolar.defanslar[1]], [280,100,kadrolar.defanslar[2]],  
+                    [100,180,kadrolar.ortasahalar[0]],  [240,180,kadrolar.ortasahalar[1]], 
+                    [170,260,kadrolar.forvetler[0]],
+                    [170,370,kadrolar.forvetler[1]], 
+                    [240,450,kadrolar.ortasahalar[2]],[60,450,kadrolar.ortasahalar[3]], 
+                    [60,530,kadrolar.defanslar[3]], [170,530,kadrolar.defanslar[4]], [280,530,kadrolar.defanslar[5]],
+                    [170,608,kadrolar.kaleciler[1]]
+                    ],
+                    [[170,32,kadrolar.kaleciler[0]],
+                    [100,100,kadrolar.defanslar[0]], [240,100,kadrolar.defanslar[1]],
+                    [60,180,kadrolar.ortasahalar[0]],  [170,180,kadrolar.ortasahalar[1]], [280,180,kadrolar.ortasahalar[2]],
+                    [170,260,kadrolar.forvetler[0]],
+                    [170,370,kadrolar.forvetler[1]], 
+                    [60,450,kadrolar.ortasahalar[3]],  [170,450,kadrolar.ortasahalar[4]],[280,450,kadrolar.ortasahalar[5]],
+                    [100,530,kadrolar.defanslar[2]], [240,530,kadrolar.defanslar[3]],
+                    [170,608,kadrolar.kaleciler[1]]],
+                    ];
+                    
 var circle = svg.selectAll("circle")
-    .data([
-        [170,32],
-        [100,100], [240,100],
-        [60,180],  [280,180],
-        [100,260], [240,260],
-        [100,370], [240,370],
-        [60,450],  [280,450],
-        [100,530], [240,530],
-        [170,608]
-        ]);
+    .data(fieldOrder[orderState]);
 var text = svg.selectAll("text")
-    .data([
-        [170,32,kadrolar.kaleciler[0]],
-        [100,100,kadrolar.defanslar[0]], [240,100,kadrolar.defanslar[1]],
-        [60,180,kadrolar.ortasahalar[0]],  [280,180,kadrolar.ortasahalar[1]],
-        [100,260,kadrolar.forvetler[0]], [240,260,kadrolar.forvetler[1]],
-        [100,370,kadrolar.forvetler[2]], [240,370,kadrolar.forvetler[3]],
-        [60,450,kadrolar.ortasahalar[2]],  [280,450,kadrolar.ortasahalar[3]],
-        [100,530,kadrolar.defanslar[2]], [240,530,kadrolar.defanslar[3]],
-        [170,608,kadrolar.kaleciler[1]]
-        ]);
-
+    .data(fieldOrder[orderState]);
     
     
     
@@ -46,6 +82,8 @@ text.enter().append("text")
 .text(function(d) {
       return d[2];
     }); 
+    $("button").attr("disabled", true);
+    
 });
 
 
@@ -59,7 +97,7 @@ function createTeams(){
     kadrolar.forvetler = $("#inpFor").val().split(/\n/); 
     karistir(kadrolar.forvetler);
     
-    
+ 
 }
 
 
